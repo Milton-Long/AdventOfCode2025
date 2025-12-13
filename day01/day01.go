6d2a30418dd155e7a88fb1fffeaff2417389c5f7
Milton Long <miltonlong17@gmail.com>
2025-12-13 05:27:41 -0500

    working on day one

--- day01/day01.go
+++ day01/day01.go
@@ -1,9 +1,28 @@
 package main
 
 import(
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 )
 
 func main(){
-	fmt.Println("Smile Bomb!")
+//	fmt.Println("Smile Bomb!")
+	input, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatalf("failed to open file: %s", err)
+	}
+	defer input.Close()
+
+	reader := bufio.NewScanner(input)
+	for reader.Scan(){
+		line := reader.Text()
+		fmt.Println(line)
+	}
+
+	if err := reader.Err(); err != nil {
+		log.Fatal(err)
+	}
+
 }
